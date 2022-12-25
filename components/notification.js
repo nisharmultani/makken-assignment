e94a4917c547b54318cc2c8bcf4eebb9288606cf
NisharMultani <nisharmultani7@gmail.com>
2022-12-25 15:41:55 +0530

    fix: notification style

--- components/notification.js
+++ components/notification.js
@@ -2,7 +2,7 @@ import style from "../styles/noti.module.css";
 export default function Notification() {
   return (
     <div className={`${style.div}`}>
-    <h4 className="bi bi-exclamation-triangle-fill color-white">Website is under construction</h4>
+    <span className="bi bi-exclamation-triangle-fill">Website is under construction</span>
     </div>
   );
 }
